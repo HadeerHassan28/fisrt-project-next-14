8cfcbe2ea9c46dbf04a62e733411260064bec18a
Hadeer Hassan <121343340+HadeerHassan28@users.noreply.github.com>
2024-02-22 13:21:49 +0200

    finish styling

--- src/component/NavBar/links/Links.jsx
+++ src/component/NavBar/links/Links.jsx
@@ -1,6 +1,6 @@
-import { sendError } from "next/dist/server/api-utils";
-import Link from "next/link";
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import styles from "./links.module.css";
 import NavLink from "./navBarLink/navLink";
 const Links = () => {
@@ -10,22 +10,43 @@ const Links = () => {
     { title: "Contact", path: "/contact" },
     { title: "Blog", path: "/blog" },
   ];
-
+  const [open, setopen] = useState(false);
   const session = true;
   const isAdmin = true;
-  return (
-    <div className={styles.links}>
-      {links.map((ele) => (
-        <NavLink item={ele} key={ele.title} />
-      ))}
-      {session ? (
-        <>
-          {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
 
-          <button className={styles.logout}>Logout</button>
-        </>
-      ) : (
-        <NavLink item={{ title: "Login", path: "/login" }} />
+  // function for small  screen menu open and close
+  const handleClick = (e) => {
+    e.preventDefault();
+    setopen(!open);
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((ele) => (
+          <NavLink item={ele} key={ele.title} />
+        ))}
+        {session ? (
+          <>
+            {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+
+            <button className={styles.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLink item={{ title: "Login", path: "/login" }} />
+        )}
+      </div>
+
+      {/* Burger button for small screen */}
+      <button className={styles.menuBtn} onClick={(prev) => handleClick(prev)}>
+        Menu
+      </button>
+
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((ele) => (
+            <NavLink item={ele} key={ele.title} />
+          ))}
+        </div>
       )}
     </div>
   );
