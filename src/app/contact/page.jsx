c0d7f808e56ccf6643b7695f673f6f272644e235
Hadeer Hassan <121343340+HadeerHassan28@users.noreply.github.com>
2024-02-24 15:52:35 +0200

    contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,23 @@
+import Image from "next/image";
 import React from "react";
-
+import styles from "./contact.module.css";
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
+    <div className={styles.container}>
+      {/* left */}
+      <div className="imgContaier">
+        <Image fill src="/contact.png" className={styles.img} alt="Conatct" />
+      </div>
+      {/* right */}
+      <div className="formContaier">
+        <form action="" className={styles.form}>
+          <input placeholder="Name and Surname" type="text" />
+          <input placeholder="Email Address" type="text" />
+          <input placeholder="Phone Number (Optional)" type="number" />
+          <textarea name="" id="" cols={30} rows="10" placeholder="Massage" />
+          <button>Send</button>
+        </form>
+      </div>
     </div>
   );
 };
