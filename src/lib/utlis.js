a50513cc487274b635adac313c2c9145d8a20006
Hadeer Hassan <121343340+HadeerHassan28@users.noreply.github.com>
2024-02-28 22:25:21 +0200

    start full stack pro mongoDb

---
+++ src/lib/utlis.js
@@ -0,0 +1,14 @@
+const connection = {};
+export const connectToDb = async () => {
+  try {
+    if (connection.isConnected) {
+      console.log("Connect to db");
+      return;
+    }
+    const db = await mongoose.connect(process.env.MONGO);
+    connection.isConnected = db.connection[0].readyState;
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+};
